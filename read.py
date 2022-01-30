b982f58e4560f6e48ebe0d2147f7fbe8e2882b1b
sushantsurwase8421 <62616518+sushantsurwase8421@users.noreply.github.com>
2022-01-30 20:22:15 +0530

    First Commit
    First Commit

---
+++ read.py
@@ -0,0 +1,14 @@
+from connection import getConnection
+
+class Read:
+    def func_ReadData(self):
+        # Get the sql connection
+        connection = getConnection()
+        cursor = connection.cursor()
+
+        # Execute the sql query
+        cursor.execute('Select * from Employee')
+
+        # Print the data
+        for row in cursor:
+            print('row = %r' % (row,))
